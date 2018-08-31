46c654a8975dfd3128694d2474ee304bfeb1bd96
Sm0keySa1m0n <joseph.tarbit@recastproductions.com>
2018-08-31 23:39:55 +0100

    Uploaded source - gradle will be added later

---
+++ com/recastproductions/network/impl/PacketHeartbeat.java
@@ -0,0 +1,19 @@
+package com.recastproductions.network.impl;
+
+import com.recastproductions.network.packet.IPacket;
+
+import io.netty.buffer.ByteBuf;
+
+public class PacketHeartbeat implements IPacket {
+
+	@Override
+	public void toBytes(ByteBuf out) throws Exception {
+		;
+	}
+
+	@Override
+	public void fromBytes(ByteBuf in) throws Exception {
+		;
+	}
+
+}
